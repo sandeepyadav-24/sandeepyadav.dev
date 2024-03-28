f2d4dcdad9d7c2b398d7a0c717a5cdab9a1d6dfd
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2024-03-28 21:31:23 +0530

    Work Ex Update

--- src/components/ListTemplate.tsx
+++ src/components/ListTemplate.tsx
@@ -6,12 +6,13 @@ interface ListTemplateProps {
 }
 const ListTemplate: React.FC<ListTemplateProps> = (props) => {
   return (
-    <div className="flex flex-row justify-between my-2" key={props.key}>
-      <div className="w-[300px]">
-        <h1 className="font-semibold">{props.name}</h1>
-        <h1 className="text-[#646464]">{props.description}</h1>
+    <div className=" my-2" key={props.key}>
+      <div className="flex flex-row justify-between">
+        <div className="font-semibold">{props.name}</div>
+        <div className="bg-white my-2 px-2 py-1 rounded-md">{props.year}</div>
       </div>
-      <div className="bg-white my-2">{props.year}</div>
+
+      <h1 className="text-[#646464] w-[300px]">{props.description}</h1>
     </div>
   );
 };
